85141d75eebba96686e20ba73f75647b338aa085
90degs2infty <anon31415@posteo.org>
2023-01-06 11:15:20 +0100

    Implement helper function to collect targets

--- cmake/ExtendedDoxygen.cmake
+++ cmake/ExtendedDoxygen.cmake
@@ -14,3 +14,39 @@ define_property(
     # scope instead of TARGET scope.
     # (see the INHERITED option at https://cmake.org/cmake/help/latest/command/define_property.html)
 )
+
+# ---------------
+# Helper to collect targets
+# ---------------
+
+# collect_targets(VAR DIR)
+#
+# Recursively collects all targets defined in directory DIR
+# and stores the resulting list in variable VAR.
+#
+# Arguments:
+# - VAR: the output variable holding the list of targets
+# - DIR: the top-most directory to traverse, must be known to CMake
+#        subdirectories that are known to CMake get traversed recursively
+function(collect_targets var dir)
+
+    # Do the heavy lifting
+    set(_targets)
+    collect_targets_recursive(_targets "${dir}")
+
+    # Propagate result to caller
+    set(${var} ${_targets} PARENT_SCOPE)
+endfunction()
+
+macro(collect_targets_recursive targets dir)
+
+    # Descend into subdirectories
+    get_property(_subdirectories DIRECTORY "${dir}" PROPERTY SUBDIRECTORIES)
+    foreach(_subdir IN LISTS _subdirectories)
+        collect_targets_recursive(${targets} "${_subdir}")
+    endforeach()
+
+    # Collect targets at current directory level
+    get_property(_current_targets DIRECTORY "${dir}" PROPERTY BUILDSYSTEM_TARGETS)
+    list(APPEND ${targets} ${_current_targets})
+endmacro()
