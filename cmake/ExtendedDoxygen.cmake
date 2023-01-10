91545f25e5ef2067d7ad0ce1921eec39efe1471a
90degs2infty <anon31415@posteo.org>
2023-01-10 14:52:11 +0100

    Rewrite handling of generated files in absolutify_source

--- cmake/ExtendedDoxygen.cmake
+++ cmake/ExtendedDoxygen.cmake
@@ -109,26 +109,51 @@ endmacro()
 # a draft approximation of correct behaviour. It works for basic use-cases, but it will
 # certainly break for more complex ones.
 function(collect_sources var target)
+    list(APPEND CMAKE_MESSAGE_CONTEXT "collect_sources")
 
     get_target_property(_sources ${target} SOURCES)
-    get_target_property(_target_source_dir ${target} SOURCE_DIR)
-    get_target_property(_target_binary_dir ${target} BINARY_DIR)
 
     set(_sources_abs)
 
     foreach(_source IN LISTS _sources)
-        absolutify_source("${_source}" "${_target_source_dir}" "$_{target_binary_dir}")
-        list(APPEND _sources_abs "${_source_abs}")
+        absolutify_source(_source_absolute "${_source}" ${target})
+        list(APPEND _sources_abs "${_source_absolute}")
     endforeach()
 
     set(${var} ${_sources_abs} PARENT_SCOPE)
 endfunction()
 
-macro(absolutify_source source target_source_dir target_binary_dir)
+function(absolutify_source var source target)
+    list(APPEND CMAKE_MESSAGE_CONTEXT "absolutify_source")
+
+    get_target_property(_target_source_dir ${target} SOURCE_DIR)
+    get_target_property(_target_binary_dir ${target} BINARY_DIR)
+
+    # There are several possibilities of what we get as input in `source`.
+    # See https://cmake.org/cmake/help/latest/prop_tgt/SOURCES.html for details.
+    # All in all, we employ the following steps:
+    # 1. If `source` contains a genex, leave it as is. Genexes are expected to evaluate to absolute paths.
+    # 2. If `source` is an absolute path, leave it as is.
+    # 3. For everything else (i.e. relative paths):
+    # 3.1 Check for the file being present in the binary dir. If it is present and it has the GENERATED
+    #     property set, this file takes precedence. Return the absolute path to the
+    #     found file (relative paths for generated files are always considered to be relative to the binary dir).
+    #     If not, continue with 3.2.
+    # 3.2 Check for the file being present in the source dir and the binary dir (in this
+    #     order). Return the absolute path to the first location that points to an existing file.
+    #     The GENERATED property does not matter.
+
+    set(_source_abs)
+
     # check for genexes
     string(GENEX_STRIP "${source}" _no_genex)
 
-    if(source STREQUAL _no_genex)
+    if(NOT source STREQUAL _no_genex)
+        # there is a genex in source, according to the documentation we
+        # may assume it evaluates to an absolute path
+        # (see https://cmake.org/cmake/help/latest/prop_tgt/SOURCES.html)
+        set(_source_abs "${source}")
+    else()
         # no genex in source, path can be both relative and absolute
         cmake_path(IS_ABSOLUTE source _is_absolute)
 
@@ -137,19 +162,30 @@ macro(absolutify_source source target_source_dir target_binary_dir)
             cmake_path(NORMAL_PATH source OUTPUT_VARIABLE _source_abs)
         else()
             # Check for a generated file
-            get_source_file_property(_generated "${source}" GENERATED)
+            # Note: the GENERATED property has some known unexpected behaviours and maybe even bugs.
+            # See the following for a discussion:
+            #
+            # - https://discourse.cmake.org/t/unexpected-behavior-of-the-generated-source-file-property-and-cmp0118/3821/3
+            # - https://discourse.cmake.org/t/behavior-of-where-cmp0118s-value-is-used-is-ambiguous/4045
+            # - https://gitlab.kitware.com/cmake/cmake/-/issues/18399
+            #
+            # All in all, the current way to query the GENERATED property is to use `get_source_file_property` with
+            # - the absolute path the generated file (the file does not have to exist on disc yet)
+            # - the absolute path to the directory in which the file is introduced to CMake (i.e. the directory in
+            #   which the introducing `CMakeLists.txt` lives)
+
+            cmake_path(ABSOLUTE_PATH source BASE_DIRECTORY "${_target_binary_dir}" NORMALIZE OUTPUT_VARIABLE _generated_candidate)
+            get_source_file_property(_generated "${_generated_candidate}" DIRECTORY "${_target_source_dir}" GENERATED)
             if(_generated)
-                # File is generated, hence relative paths are always
-                # relative to the binary directory
-                cmake_path(ABSOLUTE_PATH source BASE_DIRECTORY "${target_binary_dir}" NORMALIZE OUTPUT_VARIABLE _source_abs)
+                set(_source_abs "${_generated_candidate}")
             else()
                 # the path can be relative both to the target's source and binary directory,
-                # hence we use find_file to search for the file
-                cmake_path(GET source RELATIVE_PART _source_relative)
+                # hence we use `find_file` to search for the file
+                cmake_path(GET source PARENT_PATH _source_relative)
                 cmake_path(GET source FILENAME _source_filename)
 
-                cmake_path(ABSOLUTE_PATH _source_relative BASE_DIRECTORY "${target_source_dir}" NORMALIZE OUTPUT_VARIABLE _source_source_dir)
-                cmake_path(ABSOLUTE_PATH _source_relative BASE_DIRECTORY "${target_binary_dir}" NORMALIZE OUTPUT_VARIABLE _source_binary_dir)
+                cmake_path(ABSOLUTE_PATH _source_relative BASE_DIRECTORY "${_target_source_dir}" NORMALIZE OUTPUT_VARIABLE _source_source_dir)
+                cmake_path(ABSOLUTE_PATH _source_relative BASE_DIRECTORY "${_target_binary_dir}" NORMALIZE OUTPUT_VARIABLE _source_binary_dir)
 
                 find_file(
                     _source_abs
@@ -161,13 +197,10 @@ macro(absolutify_source source target_source_dir target_binary_dir)
                 )
             endif()
         endif()
-    else()
-        # there is a genex in source, according to the documentation we
-        # may assume it evaluates to an absolute path
-        # (see https://cmake.org/cmake/help/latest/prop_tgt/SOURCES.html)
-        set(_source_abs "${source}")
     endif()
-endmacro()
+
+    set(${var} "${_source_abs}" PARENT_SCOPE)
+endfunction()
 
 # -----------------------------------------
 # Helper to collect and include directories
